a84fb37e2408f41fcef8af0646aee09b47124a12
An Tran <tranlaman@gmail.com>
2016-06-11 21:11:23 +0800

    add benchmark codes

--- src/caffe/util/blocking_queue.cpp
+++ src/caffe/util/blocking_queue.cpp
@@ -48,7 +48,8 @@ T BlockingQueue<T>::pop(const string& log_on_wait) {
 
   while (queue_.empty()) {
     if (!log_on_wait.empty()) {
-      LOG_EVERY_N(INFO, 1000)<< log_on_wait;
+        DLOG(INFO) << log_on_wait;
+//      LOG_EVERY_N(INFO, 1000)<< log_on_wait;
     }
     sync_->condition_.wait(lock);
   }
