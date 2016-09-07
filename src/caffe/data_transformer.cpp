44ac6441e1bb5f3a127f227e4768c8aadeeac7d7
antran89 <tranlaman@gmail.com>
2016-09-07 22:17:04 +0800

    support CUDNN5.0

--- src/caffe/data_transformer.cpp
+++ src/caffe/data_transformer.cpp
@@ -1,7 +1,7 @@
 #ifdef USE_OPENCV
 #include <opencv2/core/core.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #endif  // USE_OPENCV
 
 #include <string>
