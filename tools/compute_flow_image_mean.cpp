8274c9e363c87d6218d20077150c4deafc1ba9cb
antran89 <tranlaman@gmail.com>
2016-09-07 22:17:04 +0800

    support CUDNN5.0

--- tools/compute_flow_image_mean.cpp
+++ tools/compute_flow_image_mean.cpp
@@ -9,8 +9,8 @@
 #include <vector>
 #include <iostream>
 #ifdef USE_OPENCV
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #endif
 
 #include "boost/scoped_ptr.hpp"
